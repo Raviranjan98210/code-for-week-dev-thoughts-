358aff2cdbeae4a62c179a7d71d4f5a5bd2b747c
Ravi Ranjan Kumar <raviranjan98210@gmail.com>
2020-11-23 22:02:28 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Api running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
+});
