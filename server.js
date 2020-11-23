94deb41c0600b790490e8f63b95f09b7f39f77b7
Ravi Ranjan Kumar <raviranjan98210@gmail.com>
2020-11-23 22:54:13 +0530

    database connection stablished

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+// Connecting database
+connectDB();
 
 app.get("/", (req, res) => {
   res.status(200).send("Api running");
